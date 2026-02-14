cf553553fd035fc829058e77e844fea1935aa832
sidg18 <133292211+sidg18@users.noreply.github.com>
2026-02-14 23:09:44 +1100

    Update config.js

--- config.js
+++ config.js
@@ -81,7 +81,7 @@ const CONFIG = {
         musicUrl: "https://res.cloudinary.com/daw9k68ck/video/upload/v1771070639/mybabyvoicenote_f8njb9.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
-        volume: 1                        // Volume level (0.0 to 1.0)
+        volume: 0.8                        // Volume level (0.0 to 1.0)
     }
 };
 
