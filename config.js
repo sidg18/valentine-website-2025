d9e32a71492416b70c8afd3adeb9d7578f3f2f33
sidg18 <133292211+sidg18@users.noreply.github.com>
2026-02-14 22:16:36 +1100

    Update config.js

--- config.js
+++ config.js
@@ -33,7 +33,7 @@ const CONFIG = {
             nextBtn: "Next ❤️"                                         // Text for the next button
         },
         third: {
-            text: "Though we're married, Will you be my Valentine on February 14th, 2026? 🌹", // The big question!
+            text: "Will you be my Valentine? 🌹", // The big question!
             yesBtn: "Yes!",                                             // Text for "Yes" button
             noBtn: "No"                                                 // Text for "No" button
         }
@@ -50,7 +50,7 @@ const CONFIG = {
     // Messages that appear after they say "Yes!"
     celebration: {
         title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now come get your gift, a big warm hug and a huge kiss!",
+        message: "Now come get your gift, a big warm hug and lots of kisses!",
         emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
     },
 
